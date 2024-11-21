4741ccef1f5ad36f5b295e4beb7c8083f5ddc400
Adrian Gavela Rodriguez <adriangavela09@gmail.com>
2024-11-22 00:06:13 +0100

    emparejado cuando son iguales

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ let animales = document.querySelectorAll(".animal");
 let puntosJugador1 = document.getElementById("contaor1");
 let puntosJugador2= document.getElementById("contador2");
 let reinicio = document.getElementById('reiniciar');
+let iconos = document.querySelectorAll('.fa-solid');
+
 let contador = 0;
 
 contenedor.addEventListener('click', (e)=>{
@@ -11,37 +13,66 @@ contenedor.addEventListener('click', (e)=>{
         // Comprobamos que no se hayan seleccionado mas de dos casillas, si no es asi hacemos lo siguiente:
         if(contador < 1){
             e.target.classList.add('activa');
+            e.target.classList.add('tocada');
             e.target.classList.toggle('invisible');
-            contador++;  
+            contador++; 
         }else{
         // Si se ha seleccionado una segunda casilla vamos a hacer que se quiten despues de cierto tiempo, 
         // siempre y cuando contengan las casillas la clase activa
-            e.target.classList.add('activa');
-            e.target.classList.toggle('invisible');
-
-            for(let i = 0; i < animales.length; i++){
-                
-                if(animales[i].classList.contains('activa')){
-                    // se quitan los animales despues de un segundo y se resetea el contador
-                    let tiempo = setInterval(()=>{
-                        animales[i].classList.remove('activa')
-                        animales[i].classList.add('invisible')
-                        clearInterval(tiempo);
-                    },1000)
-                    contador = 0;
-                    
-                    // Prueba medio fallida --->
-                    // if(e.target.firstElementChild.classList[2] == animales[i].firstElementChild.classList[2]){
-                    //     animales[i].classList.remove('activa')
-                    // }else{
-                    // } <----
+            
+            e.target.classList.remove('invisible'); 
+             e.target.classList.add('activa');
+             let t= setInterval(()=>{
+                                    
+                                    clearInterval(t);
+                                },1000);
+        
+                for(let k = 0; k < iconos.length; k++){
+                    //alert(e.target.firstElementChild.classList[2]);
+                    // console.log(e);
+                    // alert(iconos[k].classList[2]);
+                     //alert(animales[k].classList);
+                    if(e.target.firstElementChild.classList[2] == iconos[k].classList[2] && animales[k].classList.contains('tocada')){
+                        alert("Somos iguales");
+                        console.log("somos iguales");
+                        e.target.classList.add('completa');
+                        animales[k].classList.add('completa');
+                           e.target.classList.remove('activa');
+                        animales[k].classList.remove('activa');
+                        animales[k].classList.remove('tocada');
+                        contador=0;
+                        break;
+                    }
                 }
-                
-            }
+                if(!e.target.classList.contains('completa')){
+
+/*
+                        for(let w = 0; w < animales.length; w++){
+                            // console.log(animales);
+
+                            if(animales[w].classList.contains('tocada')){
+                                let tiempo = setInterval(()=>{
+                                    animales[w].classList.add('invisible');
+                                    animales[w].classList.remove('activa');
+                                    animales[w].classList.remove('tocada');
+                                    clearInterval(tiempo);
+                                },1000);
+                                contador = 0;
+                            }
+
+                        }
+                        e.target.classList.add('invisible');
+                        e.target.classList.remove('activa');
+  */              }            
+                       
+
+           
+
+           
         }
-    console.log(e.target.firstElementChild.classList[2]);        
+
+       
     }
-    
 })
 
 reinicio.addEventListener('click', (e)=>{
