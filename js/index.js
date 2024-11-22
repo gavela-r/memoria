9433b76cd3441aeb15d4943c8ef2159ba5d7683e
Adrian Gavela <adriangavela09@gmail.com>
2024-11-22 17:19:30 +0100

    funcionando

--- js/index.js
+++ js/index.js
@@ -4,43 +4,50 @@ let puntosJugador1 = document.getElementById("contaor1");
 let puntosJugador2= document.getElementById("contador2");
 let reinicio = document.getElementById('reiniciar');
 let iconos = document.querySelectorAll('.fa-solid');
-
 let contador = 0;
-
-// window.addEventListener('DOMContentLoaded', render());
+let primeraCarta;
+let comentario = document.getElementById('comentario');
 
 contenedor.addEventListener('click', (e)=>{
     // aqui se comprueba que se selecciona la casilla
-    if(e.target.classList.contains('animal')){
+    if(e.target.classList.contains('animal') && contador < 2){
         // Comprobamos que no se hayan seleccionado mas de dos casillas, si no es asi hacemos lo siguiente:
+        
         if(contador < 1){
             e.target.classList.add('activa');
             e.target.classList.add('tocada');
             e.target.classList.toggle('invisible');
             contador++; 
-            console.log(e.target.firstElementChild.classList[2]);
+            primeraCarta = e.target.firstElementChild.classList[2];
         }else{
-        // Si se ha seleccionado una segunda casilla vamos a hacer que se quiten despues de cierto tiempo, 
-        // siempre y cuando contengan las casillas la clase activa
+            // Si se ha seleccionado una segunda casilla vamos a hacer que se quiten despues de cierto tiempo, 
+            // siempre y cuando contengan las casillas la clase activa
             
+            contador++; 
             e.target.classList.toggle('invisible'); 
             e.target.classList.add('activa');
-            for(let k = 0; k < iconos.length; k++){
-                // && animales[k].classList.contains('tocada')
-                if(e.target.firstElementChild.classList[2] === iconos[k].classList[2] ){
-                    console.log(e.target.firstElementChild.classList[2], iconos[k].classList[2]);
-                    e.target.classList.add('completa');
+            let segundaCarta = e.target.firstElementChild.classList[2];
+        
+        if(primeraCarta == segundaCarta){
+        
+            for(let k = 0; k < animales.length; k++){
+
+                if(animales[k].firstElementChild.classList[2] == primeraCarta){
+                    
                     animales[k].classList.add('completa');
-                    e.target.classList.remove('activa');
                     animales[k].classList.remove('activa');
                     animales[k].classList.remove('tocada');
-                    contador=0;
-                    break;
+
                 }
             }
-            
-            if(!e.target.classList.contains('completa')){
-                for(let w = 0; w < animales.length; w++){
+            e.target.classList.add('completa');
+            e.target.classList.remove('activa');
+            contador = 0;
+            partida();
+        }
+        
+        if(!e.target.classList.contains('completa')){
+            for(let w = 0; w < animales.length; w++){
                     if(animales[w].classList.contains('tocada')){
                         let tiempo = setInterval(()=>{
                             animales[w].classList.add('invisible');
@@ -49,8 +56,8 @@ contenedor.addEventListener('click', (e)=>{
                             clearInterval(tiempo);
                             e.target.classList.add('invisible');
                             e.target.classList.remove('activa');
+                            contador = 0;
                         },1000);
-                        contador = 0;
                     }
 
                 }
@@ -60,11 +67,13 @@ contenedor.addEventListener('click', (e)=>{
 })
 
 reinicio.addEventListener('click', (e)=>{
-    e.preventDefault();
-    
     render()
 })
-
+window.addEventListener('keypress', (e)=>{
+    if(e.key == 'r'){
+        render()
+    }
+})
 function desordenar(animales){
     for(let i = animales.length-1; i > 0; i--){
         let j = Math.floor(Math.random() * (i+1));
@@ -74,6 +83,12 @@ function desordenar(animales){
 }
 
 function render(){
+    let numeroAleatorio = Math.floor(Math.random() * 10);
+    if(numeroAleatorio > 5){
+        comentario.innerHTML = `Empieza jugando el Jugador 1`;
+    }else{
+        comentario.innerHTML = `Empieza jugando el jugador 2`;
+    }
     let animal = [];
     
     for(let i = 0; i < animales.length; i++){
@@ -82,21 +97,18 @@ function render(){
     desordenar(animal);
 
     for(let i = 0; i < animales.length; i++){
-        // animales[i].firstElementChild.remove()
-        animales[i].appendChild(animal[i])
+        animales[i].appendChild(animal[i]);
+        animales[i].classList.add('invisible');
     }
-    console.log(animales);
 }
 
 render();
-// 
-// function partida(jugador1, jugador2){
 
-
-//     // if(e.target.className == 'animal'){
-//     //     // aqui se comprueba que se selecciona la casilla
-              
-//     // }
-// }
+function partida(){
+    for(let i = 0; i < animales.length; i++){
+        
+    }
+    return;
+}
 
 // partida(puntosJugador1, puntosJugador1)
